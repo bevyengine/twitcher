41406372b6156662254610c60b78e1c968be8d4b
François Mockers <francois.mockers@vleue.com>
2026-09-06 13:48:07 +0200

    Improve runner (#101)
    * faster git commands
    
    * update wasm-bindgen when needed

--- runner.sh
+++ runner.sh
@@ -34,7 +34,7 @@ git pull
 
 cargo build --release --bin collect
 
-git clone -b queue git@github.com:bevyengine/twitcher.git queue
+git clone --depth 1 --single-branch -b queue git@github.com:bevyengine/twitcher.git queue
 gitref=$(
     for suite in $suites; do
         [ -d "queue/$suite" ] || continue
@@ -58,13 +58,24 @@ for suite in $suites; do
 done
 run_suites_csv=$(echo "$run_suites" | tr ' ' ',')
 
-git clone git@github.com:bevyengine/bevy.git
+git init -q bevy
 cd bevy
-git reset --hard "$gitref"
+git remote add origin git@github.com:bevyengine/bevy.git
+git fetch -q --depth 1 origin "$gitref"
+git checkout -q --detach FETCH_HEAD
+want_wb=$(cargo metadata --format-version 1 2>/dev/null \
+    | jq -r '[.packages[] | select(.name=="wasm-bindgen") | .version] | first // empty')
+have_wb=$(wasm-bindgen --version 2>/dev/null | awk '{print $2}')
+if [ -z "$want_wb" ]; then
+    echo "warning: could not resolve the wasm-bindgen version; wasm size metrics may be skipped" >&2
+elif [ "$want_wb" != "$have_wb" ]; then
+    echo "wasm-bindgen-cli: have '${have_wb:-none}', need '$want_wb' - installing" >&2
+    cargo install wasm-bindgen-cli --version "$want_wb"
+fi
 ../target/release/collect --suites "$run_suites_csv" all
 cd ..
 
-git clone -b results git@github.com:bevyengine/twitcher.git results
+git clone --depth 1 --single-branch -b results git@github.com:bevyengine/twitcher.git results
 cp -r bevy/results/* results
 cd results
 git add .
@@ -73,7 +84,8 @@ git push
 cd ..
 
 cd queue
-git pull
+git fetch -q --depth 1 origin queue
+git reset -q --hard FETCH_HEAD
 for suite in $run_suites; do
     rm -f "$suite/$gitref"
 done
